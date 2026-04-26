20f18c2e5f66fe752ee7efa8bde88eccfed0f512
jin605 <jinddd3@gmail.com>
2026-04-26 18:31:48 +0900

    chore:be auth init

---
+++ be-authpay-core/src/main/java/com/jin605/authpaycore/BeAuthpayCoreApplication.java
@@ -0,0 +1,13 @@
+package com.jin605.authpaycore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeAuthpayCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeAuthpayCoreApplication.class, args);
+    }
+
+}
---
+++ be-authpay-core/src/test/java/com/jin605/authpaycore/BeAuthpayCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jin605.authpaycore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeAuthpayCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
