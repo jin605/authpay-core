4043460258c1b730317ff1fac1ac9ee5b96af158
jin605 <jinddd3@gmail.com>
2026-04-27 00:54:33 +0900

    feat : user, user mapper 추가

---
+++ be-authpay-core/src/main/java/com/jin605/authpaycore/user/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.jin605.authpaycore.user.mapper;
+
+import com.jin605.authpaycore.user.model.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public class UserMapper {
+
+    User findById(@Param("id") Long id);
+    User findByEmail(@Param("email") String email);
+    User findByNickname(@Param("nickname") String nickname);
+    int insert(User user);
+
+}
---
+++ be-authpay-core/src/main/java/com/jin605/authpaycore/user/model/User.java
@@ -0,0 +1,27 @@
+package com.jin605.authpaycore.user.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String id;
+    private String name;
+    private String nickname;
+    private String email;
+    private String passwordHash;
+    private String imageUrl;
+    private String role;
+    private String status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
