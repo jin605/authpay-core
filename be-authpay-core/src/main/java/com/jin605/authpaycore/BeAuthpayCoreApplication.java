2be5255286c9bc02053e4d2cd3c863c40361abc1
jin605 <jinddd3@gmail.com>
2026-04-26 19:07:13 +0900

    feat : 의존성 주입, jwt config 추가

--- be-authpay-core/src/main/java/com/jin605/authpaycore/BeAuthpayCoreApplication.java
+++ be-authpay-core/src/main/java/com/jin605/authpaycore/BeAuthpayCoreApplication.java
@@ -2,8 +2,10 @@ package com.jin605.authpaycore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class BeAuthpayCoreApplication {
 
     public static void main(String[] args) {
